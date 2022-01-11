3d765895da15c88e6f89251f0ae30ce1e8dbd029
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-01-11 22:31:28 +0530

    Time: 0 ms (100.00%), Space: 37.6 MB (25.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,6 +2,20 @@ class Solution {
     public int fib(int n) {
         if(n<2)
             return n;
-        return fib(n-1) + fib(n-2);
+        
+        int[] table = new int[n+1];
+        table[0] = 0;
+        table[1] = 1;
+
+        for(int i = 2; i<table.length; ++i)           
+            table[i] = table[i-1] + table[i-2];
+
+        return table[n];
     }
 }
+
+/*
+if(n<2)
+    return n;
+return fib(n-1) + fib(n-2);
+*/
