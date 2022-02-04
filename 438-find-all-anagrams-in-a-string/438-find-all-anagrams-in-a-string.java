6d3501c8c4f791132a6737bb273988dbf3c0b239
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-02-04 19:41:50 +0530

    Time: 49 ms (29.42%), Space: 48.6 MB (19.90%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,5 +1,48 @@
 class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
+    public List<Integer> findAnagrams(String s, String t) {
+        List<Integer> result = new LinkedList<>();
+        if(t.length()> s.length()) return result;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+        
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+        
+        
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+            
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0){
+                        counter++;
+                    }
+                }
+                if(end-begin == t.length()){
+                    result.add(begin);
+                }
+                begin++;
+            }
+            
+        }
+        return result;
+    }
+}
+
+
+
+/* Using 2 arrays to hold the frequency of the characters
         List<Integer> result = new ArrayList<Integer>();
         int size_s = s.length(), size_p = p.length();
         if(size_p>size_s)
@@ -27,5 +70,4 @@ class Solution {
         }
         
         return result;
-    }
-}
+*/
