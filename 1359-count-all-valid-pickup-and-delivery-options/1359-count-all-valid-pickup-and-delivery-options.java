f2574e0a6f17450e5fe9b31a8f979df2a3dfb403
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-03-06 11:34:54 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (64.02%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,14 +1,26 @@
 class Solution {
-    private int mod = (int) Math.pow(10,9) + 7;
-    long[] dp = new long[501];
-    public int countOrders(int n) {
-        dp[1]=1L;
-        dp[2]=6L;
-        for (int i=3;i<=n;i++) {
-            int spaceCount = (i-1)*2 + 1;
-            long val = (spaceCount)*(spaceCount+1)/2;
-            dp[i] = (dp[i-1]*val)%mod;
+    
+ public int countOrders(int n) {
+        long result = 1, mod = (long)1e9 + 7;
+        for(int i = 1; i <= n; i++){
+            result = result * (i * 2 - 1) * i % mod;
         }
-        return (int) dp[n];
-    }
+        return (int) result;
+    }   
+    
 }
+    
+    
+    
+//     private int mod = (int) Math.pow(10,9) + 7;
+//     long[] dp = new long[501];
+//     public int countOrders(int n) {
+//         dp[1]=1L;
+//         dp[2]=6L;
+//         for (int i=3;i<=n;i++) {
+//             int spaceCount = (i-1)*2 + 1;
+//             long val = (spaceCount)*(spaceCount+1)/2;
+//             dp[i] = (dp[i-1]*val)%mod;
+//         }
+//         return (int) dp[n];
+//     }
