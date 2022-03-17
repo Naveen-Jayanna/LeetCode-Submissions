a59e7de37046e28947f2f97566e1b7ff5d91ab6f
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-03-17 17:55:56 +0530

    Time: 1 ms (98.71%), Space: 45.6 MB (17.59%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,6 +1,26 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> st = new Stack<>(); // Create a stack
+        int i = 0; // Intialise one pointer pointing on pushed array
+        int j = 0; // Intialise one pointer pointing on popped array
+
+        for(int val : pushed){
+            pushed[i++] = val; // using pushed as the stack.
+            while(i > 0 && pushed[i - 1] == popped[j]){ // pushed[i - 1] values equal to popped[j];
+                i--; // decrement i
+                j++; // increment j
+            }
+        }
+        return i == 0; // Since pushed is a permutation of popped so at the end we are supposed to be left with an empty stack
+    }
+}
+
+
+
+/* 
+Using Stack
+
+
+Stack<Integer> st = new Stack<>(); // Create a stack
         
         int j = 0; // Intialise one pointer pointing on popped array
         
@@ -12,22 +32,4 @@ class Solution {
             }
         }
         return st.isEmpty(); // check if stack is empty return true else false
-    }
-}
-
-
-
-/* Using 2 pointers
-int i = 0; // Intialise one pointer pointing on pushed array
-        int j = 0; // Intialise one pointer pointing on popped array
-        
-        for(int val : pushed){
-            pushed[i++] = val; // using pushed as the stack.
-            while(i > 0 && pushed[i - 1] == popped[j]){ // pushed[i - 1] values equal to popped[j];
-                i--; // decrement i
-                j++; // increment j
-            }
-        }
-        return i == 0; // Since pushed is a permutation of popped so at the end we are supposed to be left with an empty stack
-        
-        */
+*/
