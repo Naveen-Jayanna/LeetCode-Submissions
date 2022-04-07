8a17d06b07ea3fdaea6f19cd0e21be519f14567e
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-04-07 13:50:47 +0530

    Time: 1 ms (98.65%), Space: 41.5 MB (61.79%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,17 +1,37 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i : stones)
-        {
-            list.add(i);
+        int c = stones.length;
+        while(c > 1){
+            Arrays.sort(stones);
+            stones[c-2]=stones[c-1] - stones[c-2];
+            c--;
         }
-        while(list.size() != 1){
-            Collections.sort(list);
-            int size = list.size();
-            int diff = Math.abs(list.get(size-1) - list.get(size-2));
-            list.set(size-2, diff);
-            list.remove(size-1);
-        }
-        return list.get(0);
+        return stones[0];
+        
     }
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // List<Integer> list = new ArrayList<Integer>();
+        // for (int i : stones)
+        // {
+        //     list.add(i);
+        // }
+        // while(list.size() != 1){
+        //     Collections.sort(list);
+        //     int size = list.size();
+        //     int diff = Math.abs(list.get(size-1) - list.get(size-2));
+        //     list.set(size-2, diff);
+        //     list.remove(size-1);
+        // }
+        // return list.get(0);
