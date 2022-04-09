82729202e8ee117853f72699f3c37cdc72b2f10f
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-04-09 15:02:53 +0530

    Time: 8 ms (97.76%), Space: 44.8 MB (90.64%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -19,3 +19,26 @@ class Solution {
         return list;
     }
 }
+
+
+/*
+
+if(k==nums.length) return nums;
+        Map<Integer, Integer>map = new HashMap<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a,b)->{return b.getValue() - a.getValue();});
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+       
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            maxHeap.add(entry);
+        }
+        int[] list = new int[k];
+        while(k>0){
+            list[k-1]=maxHeap.poll().getKey();
+            k--;
+        }
+        
+        return list;
+
+*/
