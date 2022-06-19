7613a475d4df4c4ecd61d3f6be354a1a2bb32bd2
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-06-19 23:44:32 +0530

    Time: 20 ms (89.57%), Space: 45.7 MB (92.77%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,20 +1,40 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>> ret = new ArrayList();
         Arrays.sort(products);
-        String match = "";
-        List<List<String>> result = new ArrayList<>();
-        for(char ch: searchWord.toCharArray()){
-            match += ch;
-            List<String> currRes = new ArrayList<>();
-            int count = 0;
-            for(String str: products){
-                if(str.startsWith(match + "") && count<3){
-                    currRes.add(str);
-                    count++;
+        for(int i = 1; i <= searchWord.length(); i++) {
+            List<String> toAdd = new ArrayList();
+            String key = searchWord.substring(0,i);
+            for(String product : products) {
+                if(product.length() >= i && product.indexOf(key) == 0) {
+                    toAdd.add(product);
+                    if(toAdd.size() == 3) break;
                 }
             }
-            result.add(currRes);
+            ret.add(toAdd);
         }
-        return result;
+        return ret;
     }
 }
+
+
+// class Solution {
+//     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+//         Arrays.sort(products);
+//         String match = "";
+//         List<List<String>> result = new ArrayList<>();
+//         for(char ch: searchWord.toCharArray()){
+//             match += ch;
+//             List<String> currRes = new ArrayList<>();
+//             int count = 0;
+//             for(String str: products){
+//                 if(str.startsWith(match + "") && count<3){
+//                     currRes.add(str);
+//                     count++;
+//                 }
+//             }
+//             result.add(currRes);
+//         }
+//         return result;
+//     }
+// }
