adde076e1bf0ee71aab41e79ee63d96ce1e0f269
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-06-25 00:57:51 +0530

    Time: 13 ms (74.75%), Space: 57.6 MB (51.52%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -1,26 +1,21 @@
 class Solution {
-   public boolean isPossible(int[] target) {
-	if (target.length == 1) return target[0] == 1;
-
-	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-	int sum = 0;
-	for (int t: target) {
-		pq.add(t);
-		sum += t;
-	}
-
-	while (pq.peek() != 1) {
-		int curr = pq.poll();
-		if (sum - curr == 1) return true;
-
-		int x = curr % (sum - curr);
-		sum = sum - curr + x;
-
-		if (x == 0 || x == curr) return false;
-		else pq.add(x);
-	}
-
-	return true;
-}
-    
+    public boolean isPossible(int[] A) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+        long total = 0;
+        for (int a : A) {
+            total += a;
+            pq.add(a);
+        }
+        while (true) {
+            int a = pq.poll();
+            total -= a;
+            if (a == 1 || total == 1)
+                return true;
+            if (a < total || total == 0 || a % total == 0)
+                return false;
+            a %= total;
+            total += a;
+            pq.add(a);
+        }
+    }
 }
