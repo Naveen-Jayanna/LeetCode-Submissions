3cbb191463b436feb6dc607ee69a76a71d5b39f9
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-06-26 14:12:48 +0530

    Time: 1 ms (100.00%), Space: 65.7 MB (32.50%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,15 +1,35 @@
 class Solution {
-     public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length, lSum = 0;
-        for(int i = 0; i < k; ++i){
-            lSum += cardPoints[i];
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length, l= 0, r = n - k ;
+        
+        int total = 0;
+        for (int i = r; i<n; i++) {
+            total += cardPoints[i];
         }
-        int max = lSum;
-        for(int rSum = 0, i = 0; i < k; ++i){
-            rSum += cardPoints[n-1-i];
-            lSum -= cardPoints[k-1-i];
-            max = Math.max(max,lSum+rSum);
+        int res = total;
+        while (r < n) {
+            total += (cardPoints[l] - cardPoints[r]);
+            res = Math.max(res, total);
+            l += 1;
+            r += 1;
         }
-        return max;
-    }
+        return res;
+    }    
 }
+
+
+
+
+    //  public int maxScore(int[] cardPoints, int k) {
+    //     int n = cardPoints.length, lSum = 0;
+    //     for(int i = 0; i < k; ++i){
+    //         lSum += cardPoints[i];
+    //     }
+    //     int max = lSum;
+    //     for(int rSum = 0, i = 0; i < k; ++i){
+    //         rSum += cardPoints[n-1-i];
+    //         lSum -= cardPoints[k-1-i];
+    //         max = Math.max(max,lSum+rSum);
+    //     }
+    //     return max;
+    // }
