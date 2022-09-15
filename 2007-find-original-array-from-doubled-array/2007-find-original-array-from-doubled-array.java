b5c16f0d18503161b263434c4398f79a695c2cdb
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-09-15 02:15:12 -0400

    Time: 21 ms (90.27%), Space: 125.6 MB (83.68%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,5 +1,63 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
+        if (changed.length % 2 == 1) {
+            return new int[0];
+        }
+        
+        int maxNum = 0;
+        for (int num : changed) {
+            maxNum = Math.max(maxNum, num);
+        }
+        
+        int[] freq = new int[2 * maxNum + 1];
+        for (int num : changed) {
+            freq[num]++;
+        }
+        
+        int[] original = new int[changed.length / 2];
+        int index = 0;
+        for (int num = 0; num <= maxNum; num++) {
+            if (freq[num] > 0) {
+                freq[num]--;
+                
+                int twiceNum = num * 2;
+                if (freq[twiceNum] > 0) {
+                    freq[twiceNum]--;
+                    original[index++] = num;
+                    num--;
+                } else {
+                    return new int[0];
+                }
+            }
+        }
+        
+        return original;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Using TreeMap we can solve the same way as shown above
+
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
         int n = changed.length, i = 0;
         if (n % 2 == 1) return new int[0];
         int[] res = new int[n / 2];
@@ -17,3 +75,4 @@ class Solution {
         return res;
     }
 }
+*/
