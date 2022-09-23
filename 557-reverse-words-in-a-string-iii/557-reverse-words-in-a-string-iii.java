6f1a7a7d86197296fdd758b5720e60d702553093
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-09-23 02:55:47 -0400

    Time: 12 ms (55.38%), Space: 51.7 MB (41.84%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -1,9 +1,29 @@
 class Solution {
-    public String reverseWords(String s) {
-        String[] str = s.split(" ");
-        for (int i = 0; i < str.length; i++) str[i] = new StringBuilder(str[i]).reverse().toString();
-        StringBuilder result = new StringBuilder();
-        for (String st : str) result.append(st + " ");
-        return result.toString().trim();
+    public String reverseWords(String str) {
+        String strs[] = str.split(" ");
+        for(int i = 0; i < strs.length; i++) {
+            char s[] = strs[i].toCharArray();
+            reverseString(s);
+            strs[i] = new String(s);
+        }
+        return String.join(" ",strs);
+    }
+    private void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while(left < right){
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
     }
 }
+
+
+// public String reverseWords(String s) {
+//         String[] str = s.split(" ");
+//         for (int i = 0; i < str.length; i++) str[i] = new StringBuilder(str[i]).reverse().toString();
+//         StringBuilder result = new StringBuilder();
+//         for (String st : str) result.append(st + " ");
+//         return result.toString().trim();
+//     }
