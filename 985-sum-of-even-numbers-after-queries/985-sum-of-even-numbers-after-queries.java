7fdcbd287b30e8bc71aa9b07e14a4bd742b6d606
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-09-23 20:14:42 -0400

    Time: 4 ms (99.06%), Space: 50.2 MB (93.14%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -3,9 +3,9 @@ class Solution {
         int result[] = new int[queries.length];
         int even_sum = 0;
         int j= 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%2 ==0)
-                even_sum += nums[i];
+        for(int num:nums){
+            if(num%2 ==0)
+                even_sum += num;
         }
 
         for(int i=0; i<queries.length; i++){
