a5811b045a9646f6a7fcebc15a45ca0163d1885c
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-09-27 02:57:07 -0400

    Time: 2 ms (79.69%), Space: 42.7 MB (78.46%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.java
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.java
@@ -1,19 +1,5 @@
 class Solution {
-    public int[] shortestToChar(String S, char C) {
-        int n = S.length();
-        int[] res = new int[n];
-        for (int i = 0; i < n; ++i)
-            res[i] = S.charAt(i) == C ? 0 : n;
-        for (int i = 1; i < n; ++i)
-            res[i] = Math.min(res[i], res[i - 1] + 1);
-        for (int i = n - 2; i >= 0; --i)
-            res[i] = Math.min(res[i], res[i + 1] + 1);
-        return res;
-    }
-}
-
-/*
-    public int[] shortestToChar(String S, char C) {
+     public int[] shortestToChar(String S, char C) {
         int n = S.length(), pos = -n, res[] = new int[n];
         for (int i = 0; i < n; ++i) {
             if (S.charAt(i) == C) pos = i;
@@ -23,6 +9,23 @@ class Solution {
             if (S.charAt(i) == C)  pos = i;
             res[i] = Math.min(res[i], pos - i);
         }
+        return res;
+    }
+}
+
+/*
+   
+    
+    
+    public int[] shortestToChar(String S, char C) {
+        int n = S.length();
+        int[] res = new int[n];
+        for (int i = 0; i < n; ++i)
+            res[i] = S.charAt(i) == C ? 0 : n;
+        for (int i = 1; i < n; ++i)
+            res[i] = Math.min(res[i], res[i - 1] + 1);
+        for (int i = n - 2; i >= 0; --i)
+            res[i] = Math.min(res[i], res[i + 1] + 1);
         return res;
     }
     */
