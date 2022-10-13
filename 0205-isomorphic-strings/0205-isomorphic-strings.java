83d9b69f8a996bf2b89987b39ef45a419698b6d4
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-10-12 23:24:01 -0400

    Time: 22 ms (40.19%), Space: 43 MB (49.35%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int arr1[] = new int[128];
+        int arr2[] = new int[128];
+        for(int i=0; i<s.length();i++){
+            if(arr1[s.charAt(i)] != 0 && arr1[s.charAt(i)] != t.charAt(i))
+                return false;
+            if(arr2[t.charAt(i)] != 0 && arr2[t.charAt(i)] != s.charAt(i))
+                return false;
+            arr1[s.charAt(i)] = t.charAt(i);
+            arr2[t.charAt(i)] = s.charAt(i);
+        }
+        return true;
+}
+}
