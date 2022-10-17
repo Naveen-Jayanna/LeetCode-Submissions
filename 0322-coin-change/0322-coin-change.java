cf1b960adec40fc89f72447c4e62e996b8e86a27
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2022-10-16 20:47:15 -0400

    Time: 31 ms (57.66%), Space: 45.6 MB (33.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,7 +5,10 @@ class Solution {
         dp[0] = 0; 
         for(int j = 0; j < coins.length; j++){
             for(int i = 0; i <= amount; i++){
-                if(i - coins[j] >= 0) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1); 
+                if(i - coins[j] >= 0)
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1); 
+                else
+                    i = coins[j] - 1;
             } 
         } 
         return dp[amount] > amount ? -1 : dp[amount];
