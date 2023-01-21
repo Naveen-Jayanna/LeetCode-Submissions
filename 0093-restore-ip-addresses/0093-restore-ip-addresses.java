70464558f168a25dc2e9d0c9197f3f5352893d6a
Naveen Jayanna <52947925+Naveen-Jayanna@users.noreply.github.com>
2023-01-21 01:26:59 -0500

    Time: 6 ms (54.94%), Space: 42.1 MB (85.64%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -11,7 +11,7 @@ class Solution {
                 result.add(path.substring(1));
             return;
         }
-        for (int i = 1; i <= (s.charAt(0) == '0' ? 1 : 3) && i <= s.length(); i++) { // Avoid leading 0
+        for (int i = 1; i <= (s.charAt(0) == '0' ? 1 : 3) && i <= s.length(); i++) {
             String part = s.substring(0, i);
             if (Integer.valueOf(part) <= 255)
                 doRestore(result, path + "." + part, s.substring(i), k + 1);
